5587d48370588aca06fd973908c738473277272f
Vivek Misra <115539722+vimis22@users.noreply.github.com>
2024-12-13 14:00:56 +0100

    Working on JUnitTest for JHotDraw Framework.

--- jhotdraw-gui/src/test/java/org/jhotdraw/gui/JFontChooserTest.java
+++ jhotdraw-gui/src/test/java/org/jhotdraw/gui/JFontChooserTest.java
@@ -1,23 +1,32 @@
 package org.jhotdraw.gui;
 
 import org.jhotdraw.gui.fontchooser.ExperimentDefaultChooserModel;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
+import javax.swing.*;
 import java.awt.*;
 
 import static org.junit.Assert.*;
 
 public class JFontChooserTest {
     JFontChooser fontChooser;
+    JLabel label;
+    Font font;
     ExperimentDefaultChooserModel defaultChooserModel;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         fontChooser = new JFontChooser();
         defaultChooserModel = new ExperimentDefaultChooserModel();
+        fontChooser.setModel(defaultChooserModel);
         System.out.println("Setup is executed");
+    }
+
+    @BeforeAll
+    public static void checkSetup() throws Exception {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
     }
 
     @Test
@@ -25,7 +34,7 @@ public class JFontChooserTest {
         Font fontSelection = fontChooser.getSelectedFont();
         if(fontSelection == null){
             fontChooser.setSelectedFont(null);
-        } else if (fontSelection != null){
+        } else {
             fontChooser.setSelectedFont(Font.getFont(fontSelection.getName()));
         }
     }
@@ -39,6 +48,35 @@ public class JFontChooserTest {
         }else {
             assertNotEquals("These are two different fonts", oldvalue.getFontName(), newvalue.getFontName());
         }
+    }
+
+    @Test
+    public void test_if_fontselection_applied(){
+        JLabel mockLabel = new JLabel("Hello World to MockLabel");
+        Font mockFont = new Font("Times New Roman", Font.BOLD, 12);
+        fontChooser.setSelectedFont(mockFont);
+        if (fontChooser.getSelectedFont() != null){
+            Font implementedFont = fontChooser.getSelectedFont();
+            mockLabel.setFont(implementedFont);
+            assertEquals("The Font is now applied to the label", mockFont.equals(mockLabel.getFont()));
+        } else if ( fontChooser.getSelectedFont() == null) {
+            assertNotEquals("The Font is not applied to the label", mockFont.equals(mockLabel.getFont()));
+        }
+    }
+
+    @Test
+    public void test_fontselection_in_display() throws Exception {
+        SwingUtilities.invokeAndWait(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.getContentPane().add(fontChooser);
+                frame.pack();
+                frame.setVisible(true);
+                assertNotNull(fontChooser);
+            }
+        });
     }
 
 }
