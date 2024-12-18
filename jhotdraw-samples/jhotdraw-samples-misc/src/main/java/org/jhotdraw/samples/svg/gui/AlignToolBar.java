391cba65d534c16920ff1f78f7915f9662c4660b
FearfulSnow <11558593+FearfulSnow@users.noreply.github.com>
2024-12-18 10:17:18 +0100

    Code smell, long method = 3 methods

--- jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/svg/gui/AlignToolBar.java
+++ jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/svg/gui/AlignToolBar.java
@@ -9,6 +9,8 @@ package org.jhotdraw.samples.svg.gui;
 
 import org.jhotdraw.gui.plaf.palette.PaletteButtonUI;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.*;
 import javax.swing.border.*;
 import org.jhotdraw.draw.DrawingEditor;
@@ -49,79 +51,68 @@ public class AlignToolBar extends AbstractToolBar {
         }
     }
 
+    private static class AlignActionButtonData {
+        final AbstractSelectedAction action;
+        final int gridx;
+        final int gridy;
+        final Insets insets;
+
+        AlignActionButtonData(AbstractSelectedAction action, int gridx, int gridy, Insets insets) {
+            this.action = action;
+            this.gridx = gridx;
+            this.gridy = gridy;
+            this.insets = insets;
+        }
+    }
+
     @Override
     protected JComponent createDisclosedComponent(int state) {
-        JPanel p = null;
-        if (state == 1) {
-            p = new JPanel();
-            p.setOpaque(false);
-            p.setBorder(new EmptyBorder(5, 5, 5, 8));
-            // Abort if no editor is set
-            if (editor == null) {
-                return p;
-            }
-            ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.samples.svg.Labels");
-            GridBagLayout layout = new GridBagLayout();
-            p.setLayout(layout);
-            GridBagConstraints gbc;
-            AbstractButton btn;
-            AbstractSelectedAction alignAction;
-            String clientPropertyKey = "hideActionText";
+        JPanel panel = null;
+        if (state == 1) return panel;
+        // Define the panel
+        panel = new JPanel();
+        panel.setOpaque(false);
+        panel.setBorder(new EmptyBorder(5, 5, 5, 8));
+        // Abort if no editor is set
+        if (editor == null) return panel;
 
-            gbc = new GridBagConstraints();
-            gbc.gridy = 0;
-            alignAction = new AlignAction.West(editor, labels);
-            btn = new JButton(alignAction);
-            disposables.add(alignAction);
-            btn.setUI((PaletteButtonUI) PaletteButtonUI.createUI(btn));
-            btn.setText(null);
-            p.add(btn, gbc);
-            gbc.insets = new Insets(0, 3, 0, 0);
-            alignAction = new AlignAction.East(editor, labels);
-            btn = new JButton(alignAction);
-            disposables.add(alignAction);
-            btn.setUI((PaletteButtonUI) PaletteButtonUI.createUI(btn));
-            btn.putClientProperty(clientPropertyKey, Boolean.TRUE);
-            btn.setText(null);
-            p.add(btn, gbc);
-            gbc.gridy = 1;
-            gbc.insets = new Insets(3, 0, 0, 0);
-            alignAction = new AlignAction.North(editor, labels);
-            btn = new JButton(alignAction);
-            disposables.add(alignAction);
-            btn.setUI((PaletteButtonUI) PaletteButtonUI.createUI(btn));
-            btn.putClientProperty(clientPropertyKey, Boolean.TRUE);
-            btn.setText(null);
-            p.add(btn, gbc);
-            gbc.insets = new Insets(3, 3, 0, 0);
-            alignAction = new AlignAction.South(editor, labels);
-            btn = new JButton(alignAction);
-            disposables.add(alignAction);
-            btn.setUI((PaletteButtonUI) PaletteButtonUI.createUI(btn));
-            btn.putClientProperty(clientPropertyKey, Boolean.TRUE);
-            btn.setText(null);
-            p.add(btn, gbc);
-            gbc.gridx = 0;
-            gbc.gridy = 2;
-            gbc.insets = new Insets(3, 0, 0, 0);
-            alignAction = new AlignAction.Horizontal(editor, labels);
-            btn = new JButton(alignAction);
-            disposables.add(alignAction);
-            btn.setUI((PaletteButtonUI) PaletteButtonUI.createUI(btn));
-            btn.putClientProperty(clientPropertyKey, Boolean.TRUE);
-            btn.setText(null);
-            p.add(btn, gbc);
-            gbc.gridx = 1;
-            gbc.insets = new Insets(3, 3, 0, 0);
-            alignAction = new AlignAction.Vertical(editor, labels);
-            btn = new JButton(alignAction);
-            disposables.add(alignAction);
-            btn.setUI((PaletteButtonUI) PaletteButtonUI.createUI(btn));
-            btn.putClientProperty(clientPropertyKey, Boolean.TRUE);
-            btn.setText(null);
-            p.add(btn, gbc);
+        ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.samples.svg.Labels");
+        GridBagLayout layout = new GridBagLayout();
+        panel.setLayout(layout);
+
+        GridBagConstraints gbc;
+        gbc = new GridBagConstraints();
+
+        // list of align actions and their grid positions
+        List<AlignActionButtonData> alignButtonsData = new ArrayList<>();
+        alignButtonsData.add(new AlignActionButtonData(new AlignAction.West(editor, labels), 0, 0, new Insets(0, 0, 0, 0)));
+        alignButtonsData.add(new AlignActionButtonData(new AlignAction.East(editor, labels), 1, 0, new Insets(0, 3, 0, 0)));
+        alignButtonsData.add(new AlignActionButtonData(new AlignAction.North(editor, labels), 0, 1, new Insets(3, 0, 0, 0)));
+        alignButtonsData.add(new AlignActionButtonData(new AlignAction.South(editor, labels), 1, 1, new Insets(3, 3, 0, 0)));
+        alignButtonsData.add(new AlignActionButtonData(new AlignAction.Horizontal(editor, labels), 0, 2, new Insets(3, 0, 0, 0)));
+        alignButtonsData.add(new AlignActionButtonData(new AlignAction.Vertical(editor, labels), 1, 2, new Insets(3, 3, 0, 0)));
+
+        // add an align button for each align action
+        for (AlignActionButtonData alignButtonData : alignButtonsData) {
+            addAlignActionButton(panel, gbc, alignButtonData);
         }
-        return p;
+        return panel;
+    }
+
+    private void addAlignActionButton(JPanel panel, GridBagConstraints gbc, AlignActionButtonData alignButtonData) {
+        // Grid
+        gbc.gridx = alignButtonData.gridx;
+        gbc.gridy = alignButtonData.gridy;
+        gbc.insets = alignButtonData.insets;
+        // Button
+        AbstractButton btn = new JButton(alignButtonData.action);
+        disposables.add(alignButtonData.action);
+
+        btn.setUI((PaletteButtonUI) PaletteButtonUI.createUI(btn));
+        btn.putClientProperty("hideActionText", Boolean.TRUE);
+        btn.setText(null);
+        // Add grid and button to panel
+        panel.add(btn, gbc);
     }
 
     /**
